0c5211a7e8d118958ac8d7be94dc8f02d65d7c22
jeen0404 <jivan0404patel@gmail.com>
2021-03-23 12:10:08 +0530

    adding happy holi template

--- raindrop/urls.py
+++ raindrop/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from.import views
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('',views.holi,name='holi'),
